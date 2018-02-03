fbed535838cd6274bb379115d1e1cea401bae7b6
Dong Chen <dongchenchn@gmail.com>
2018-02-03 11:40:07 +0000

    add carrierwave

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,5 @@ class Item < ApplicationRecord
     belongs_to :user
     has_many :messages
     has_many :itempics
-<<<<<<< HEAD
     mount_uploader :picture, AvatarUploader
-=======
-    mount_uploader :picure, AvatarUploader
->>>>>>> 2672eaf58b45f38d4eccadfec639c3baff054ce9
 end
