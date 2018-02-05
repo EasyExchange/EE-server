4db768ddc9fa1662fcb66dc43d46c7db9c0b430c
Dong Chen <dongchenchn@gmail.com>
2018-02-05 13:07:04 +0000

    modify some as_json

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,14 +3,14 @@ class Message < ApplicationRecord
     belongs_to :receiver, class_name: "User"
     belongs_to :item
     
-    def as_json(options = {})
-      if options
-        super
-      else
-        super.merge(
-          :item => self.item,
-          :sender => self.sender
-        )
-      end
-    end
+    #def as_json(options = {})
+    #  if options
+    #    super
+    #  else
+    #    super.merge(
+    #      :item => self.item,
+    #      :sender => self.sender
+    #    )
+    #  end
+    #end
 end
